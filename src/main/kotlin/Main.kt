6e96cbc820bcfaf43ed6848012062174f7b4b0ce
Lss233 <8984680+lss233@users.noreply.github.com>
2022-10-01 01:10:13 +0800

    networking: separate bootstrap with Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,58 +1,5 @@
-import com.lss233.minidb.networking.NettyServerInitializer
-import io.netty.bootstrap.ServerBootstrap
-import io.netty.channel.Channel
-import io.netty.channel.ChannelOption
-import io.netty.channel.EventLoopGroup
-import io.netty.channel.nio.NioEventLoopGroup
-import io.netty.channel.socket.nio.NioServerSocketChannel
-import java.net.InetSocketAddress
-class NettyServer {
+import com.lss233.minidb.networking.NettyServer
 
-    private var channel: Channel?=null
-    private lateinit var bossGroup: EventLoopGroup
-    private lateinit var workerGroup: EventLoopGroup
-
-    var port = 5432
-        set(value)  {
-            field = value
-        }
-
-    fun start() {
-        object : Thread() {
-            override fun run() {
-                super.run()
-                bossGroup = NioEventLoopGroup(1)
-                workerGroup = NioEventLoopGroup()
-                try {
-                    val b = ServerBootstrap()
-                    b.group(bossGroup, workerGroup)
-                        .channel(NioServerSocketChannel::class.java)
-                        .localAddress(InetSocketAddress(port))
-                        .childOption(ChannelOption.SO_KEEPALIVE, true)
-                        .childOption(ChannelOption.SO_REUSEADDR, true)
-                        .childOption(ChannelOption.TCP_NODELAY, true)
-                        .childHandler(NettyServerInitializer())
-
-                    // Bind and start to accept incoming connections.
-                    val f = b.bind().sync()
-
-                    f.channel().closeFuture().sync()
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                } finally {
-                    disconnect()
-                }
-            }
-        }.start()
-
-    }
-
-    fun disconnect() {
-        workerGroup.shutdownGracefully()
-        bossGroup.shutdownGracefully()
-    }
-
-}
 fun main(args: Array<String>) {
     println("MiniDB!")
 
---
+++ src/main/kotlin/com/lss233/minidb/networking/NettyServer.kt
@@ -0,0 +1,52 @@
+package com.lss233.minidb.networking
+
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.ChannelOption
+import io.netty.channel.EventLoopGroup
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import java.net.InetSocketAddress
+
+class NettyServer {
+
+    private lateinit var bossGroup: EventLoopGroup
+    private lateinit var workerGroup: EventLoopGroup
+
+    var port = 5432
+
+    fun start() {
+        object : Thread() {
+            override fun run() {
+                super.run()
+                bossGroup = NioEventLoopGroup(1)
+                workerGroup = NioEventLoopGroup()
+                try {
+                    val b = ServerBootstrap()
+                    b.group(bossGroup, workerGroup)
+                        .channel(NioServerSocketChannel::class.java)
+                        .localAddress(InetSocketAddress(port))
+                        .childOption(ChannelOption.SO_KEEPALIVE, true)
+                        .childOption(ChannelOption.SO_REUSEADDR, true)
+                        .childOption(ChannelOption.TCP_NODELAY, true)
+                        .childHandler(NettyServerInitializer())
+
+                    // Bind and start to accept incoming connections.
+                    val f = b.bind().sync()
+
+                    f.channel().closeFuture().sync()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    disconnect()
+                }
+            }
+        }.start()
+
+    }
+
+    fun disconnect() {
+        workerGroup.shutdownGracefully()
+        bossGroup.shutdownGracefully()
+    }
+
+}
