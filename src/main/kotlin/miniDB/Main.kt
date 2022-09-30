65332935ff746c23d5783b214de2cd8d9b1c2d16
icebigpig <icebigpig404@foxmail.com>
2022-09-30 11:16:57 +0800

    fix dir error

--- src/main/kotlin/miniDB/Main.kt
+++ src/main/kotlin/miniDB/Main.kt
@@ -12,9 +12,12 @@ fun main(args: Array<String>) {
         "CREATE TABLE `Test` ( Id_P int,LastName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255))"
     // 通过解析SQL后得到建表的实体类
     val ast: DDLCreateTableStatement = SQLParserDelegate.parse(sql) as DDLCreateTableStatement
+
     println("解析：创建表:" + ast.table.idText)
+
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+
