d4fa1fb4b00ccdb5f2e89495824c432c4f983e41
lss233 <i@lss233.com>
2022-11-17 16:52:53 +0800

    Cell: support equal comparison between String and Number

--- src/main/kotlin/com/lss233/minidb/engine/Cell.kt
+++ src/main/kotlin/com/lss233/minidb/engine/Cell.kt
@@ -1,6 +1,8 @@
 package com.lss233.minidb.engine
 
 import com.lss233.minidb.engine.schema.Column
+import miniDB.parser.ast.expression.primary.literal.LiteralNumber
+import miniDB.parser.ast.expression.primary.literal.LiteralString
 
 /**
  * 行式存储结构中的单体数据(单元格数据)
@@ -10,6 +12,12 @@ import com.lss233.minidb.engine.schema.Column
 class Cell<T> constructor(var column: Column, var value: T) {
 
     override fun equals(other: Any?): Boolean {
+        if (other is LiteralString) {
+            return value.toString() == other.unescapedString
+        }
+        if (other is LiteralNumber) {
+            return value.toString().toDoubleOrNull() == other.number
+        }
         return if (other is Cell<*>)
             value == other.value
         else
